a773114452f715e3751ca091921c324fc7cf4a61
noshpineapple28 <manoucherin@gmail.com>
2024-12-24 23:36:38 -0800

    feat: part 2 done

--- day6/main.c
+++ day6/main.c
@@ -13,6 +13,8 @@ uint8_t cols = 0;
 DIRECTION guard_direction = UP;
 uint8_t guard_row = 0;
 uint8_t guard_col = 0;
+// candidate for potential inf loop
+candidate cand = {0};
 
 // helper methods
 
@@ -23,21 +25,31 @@ uint8_t guard_col = 0;
  */
 void hande_up_move(TILE **grid)
 {
+    /** DETERMINE NEXT TILE TO GO TO **/
     // if direction above is blocked, set direction right
-    if (guard_row >= 1 && grid[guard_row - 1][guard_col] == BLOCKED)
+    if (guard_row >= 1 &&
+        (grid[guard_row - 1][guard_col] == BLOCKED ||
+         grid[guard_row - 1][guard_col] == INF_BLOCK))
     {
+        // update candidacy
+        if (cand.direction == INVALID && grid[guard_row - 1][guard_col] == INF_BLOCK)
+            cand.direction = guard_direction;
+        else if (cand.direction == guard_direction && grid[guard_row - 1][guard_col] == INF_BLOCK)
+            cand.direction = INF;
+
         guard_direction = RIGHT;
+        return;
     }
+
+    grid[guard_row][guard_col] = TRED;
     // if space isn't blocked move up
-    else if (guard_row >= 1)
+    if (guard_row >= 1)
     {
-        grid[guard_row][guard_col] = TRODDEN;
         guard_row--;
     }
     // if space is the border, set to exit
     else
     {
-        grid[guard_row][guard_col] = TRODDEN;
         guard_direction = EXIT;
     }
 }
@@ -49,21 +61,31 @@ void hande_up_move(TILE **grid)
  */
 void hande_right_move(TILE **grid)
 {
+    /** DETERMINE NEXT TILE TO GO TO **/
     // if direction right is blocked, set direction down
-    if (guard_col < cols - 1 && grid[guard_row][guard_col + 1] == BLOCKED)
+    if (guard_col < cols - 1 &&
+        (grid[guard_row][guard_col + 1] == BLOCKED ||
+         grid[guard_row][guard_col + 1] == INF_BLOCK))
     {
+        // set candidacy direction
+        if (cand.direction == INVALID && grid[guard_row][guard_col + 1] == INF_BLOCK)
+            cand.direction = guard_direction;
+        else if (cand.direction == guard_direction && grid[guard_row][guard_col + 1] == INF_BLOCK)
+            cand.direction = INF;
+
         guard_direction = DOWN;
+        return;
     }
+
+    grid[guard_row][guard_col] = TRED;
     // if space isn't blocked move right
-    else if (guard_col < cols - 1)
+    if (guard_col < cols - 1)
     {
-        grid[guard_row][guard_col] = TRODDEN;
         guard_col++;
     }
     // if space is the border, set to exit
     else
     {
-        grid[guard_row][guard_col] = TRODDEN;
         guard_direction = EXIT;
     }
 }
@@ -75,21 +97,31 @@ void hande_right_move(TILE **grid)
  */
 void hande_down_move(TILE **grid)
 {
+    /** DETERMINE NEXT TILE TO GO TO **/
     // if direction to below is blocked, set direction left
-    if (guard_row < rows - 1 && grid[guard_row + 1][guard_col] == BLOCKED)
+    if (guard_row < rows - 1 &&
+        (grid[guard_row + 1][guard_col] == BLOCKED ||
+         grid[guard_row + 1][guard_col] == INF_BLOCK))
     {
+        // update candidacy
+        if (cand.direction == INVALID && grid[guard_row + 1][guard_col] == INF_BLOCK)
+            cand.direction = guard_direction;
+        else if (cand.direction == guard_direction && grid[guard_row + 1][guard_col] == INF_BLOCK)
+            cand.direction = INF;
+
         guard_direction = LEFT;
+        return;
     }
+
+    grid[guard_row][guard_col] = TRED;
     // if space isn't blocked move right
-    else if (guard_row < rows - 1)
+    if (guard_row < rows - 1)
     {
-        grid[guard_row][guard_col] = TRODDEN;
         guard_row++;
     }
     // if space is the border, set to exit
     else
     {
-        grid[guard_row][guard_col] = TRODDEN;
         guard_direction = EXIT;
     }
 }
@@ -101,21 +133,30 @@ void hande_down_move(TILE **grid)
  */
 void hande_left_move(TILE **grid)
 {
+    /** DETERMINE NEXT TILE TO GO TO **/
     // if direction to left is blocked, set direction up
-    if (guard_col >= 1 && grid[guard_row][guard_col - 1] == BLOCKED)
+    if (guard_col >= 1 &&
+        (grid[guard_row][guard_col - 1] == BLOCKED ||
+         grid[guard_row][guard_col - 1] == INF_BLOCK))
     {
+        // update candidacy
+        if (cand.direction == INVALID && grid[guard_row][guard_col - 1] == INF_BLOCK)
+            cand.direction = guard_direction;
+        else if (cand.direction == guard_direction && grid[guard_row][guard_col - 1] == INF_BLOCK)
+            cand.direction = INF;
         guard_direction = UP;
+        return;
     }
+
+    grid[guard_row][guard_col] = TRED;
     // if space isn't blocked move left
-    else if (guard_col >= 1)
+    if (guard_col >= 1)
     {
-        grid[guard_row][guard_col] = TRODDEN;
         guard_col--;
     }
     // if space is the border, set to exit
     else
     {
-        grid[guard_row][guard_col] = TRODDEN;
         guard_direction = EXIT;
     }
 }
@@ -136,7 +177,7 @@ uint16_t find_tot_tiles_trodden(TILE **grid)
     {
         while (col < cols)
         {
-            tot += (grid[row][col] == TRODDEN);
+            tot += (grid[row][col] == TRED);
             col++;
         }
         col = 0;
@@ -146,6 +187,12 @@ uint16_t find_tot_tiles_trodden(TILE **grid)
     return tot;
 }
 
+/**
+ * calculates the total number of columns and rows for a given
+ *      grid in a givne file
+ * 
+ * @param fp the file we are reading from
+ */
 void calc_tot_cols_rows(FILE *fp)
 {
     char c = 0;
@@ -161,6 +208,69 @@ void calc_tot_cols_rows(FILE *fp)
 
     // move file back to start
     fseek(fp, 0, SEEK_SET);
+}
+
+/**
+ * resets the grid to an initial position and then moves the potential
+ *      infinite loop block up a space
+ * 
+ * @param grid the grid we are reseting
+ * @returns 1 if we have exceeded all possible tiles to check
+ *      0 otherwise
+ */
+uint8_t reset_grid(TILE **grid)
+{
+    uint8_t col = 0;
+    uint8_t row = 0;
+    // loop until we find a non block space   
+    do
+    {
+        // change the candidate col
+        cand.col++;
+        // prevent overreach
+        if (cand.col >= cols)
+        {
+            cand.col = 0;
+            cand.row++;
+        }
+
+        // exit loop we have checked every tile
+        if (cand.row >= rows)
+            return 1;    
+    } while (grid[cand.row][cand.col] == BLOCKED);
+    
+    // reset starter vals
+    cand.direction = INVALID;
+    cand.steps_taken = 0;
+    guard_col = cand.guard_init_col;
+    guard_row = cand.guard_init_row;
+    guard_direction = UP;
+
+    // loop through the grid and reset vals
+    while (row < rows)
+    {
+        while (col < cols)
+        {
+            // set the inf block
+            if (row == cand.row && col == cand.col)
+            {
+                grid[row][col] = INF_BLOCK;
+            }
+            // set the guard init pos
+            else if (row == cand.guard_init_row && col == cand.guard_init_col)
+                grid[row][col] = GUARD;
+            // set empty
+            else if (grid[row][col] != BLOCKED)
+                grid[row][col] = EMPTY;
+
+            // incriment
+            col++;
+        }
+        col = 0;
+        row++;
+    }
+
+    return 0;
 }
 
 // main methods
@@ -190,6 +300,9 @@ TILE **fill_grid(FILE *fp, TILE **grid)
             {
                 guard_row = row;
                 guard_col = col - 1;
+                // save on candidate
+                cand.guard_init_col = guard_col;
+                cand.guard_init_row = guard_row;
             }
         }
         // skip newline
@@ -226,6 +339,12 @@ void move_guard(TILE **grid)
     }
 }
 
+/**
+ * initiates a procedurly generated grid generated from a given file
+ * 
+ * @param fp the file to generate from
+ * @returns pointer to dynamically generated grid
+ */
 TILE **initiate_grid(FILE *fp)
 {
     TILE **grid;
@@ -240,12 +359,14 @@ TILE **initiate_grid(FILE *fp)
 
     // fille grid with tiles
     grid = fill_grid(fp, grid);
+    if (grid[0][0] != BLOCKED)
+        grid[0][0] = INF_BLOCK;
 
     return grid;
 }
 
 /**
- * runs advent of code day 5
+ * runs advent of code day 6
  */
 void main()
 {
@@ -260,11 +381,40 @@ void main()
     grid = initiate_grid(fp);
 
     // move the guard
-    while (guard_direction != EXIT)
-        move_guard(grid);
+    uint16_t tot_inf_loop_blocks = 0;
+    printf("TOTAL POSSIBLE INF LOOP TILES: %4d", tot_inf_loop_blocks);
+
+    // loop until the reset func states that it's checked every tile
+    do
+    {
+        while (guard_direction != EXIT && cand.steps_taken < 16900)
+        {
+            move_guard(grid);
+            // if the guard has ended up in the same position we have an inf loop
+            if (cand.direction == INF)
+            {
+                // animation to alert user as to what's going on
+                tot_inf_loop_blocks++;
+                printf("\b\b\b\b%4d", tot_inf_loop_blocks);
+                break;
+            }
+            // increment steps taken in case we reach an inf loop some other way
+            cand.steps_taken++;
+        }
+
+        // incriment counter for if inf loop reached without candidacy check
+        if (cand.steps_taken >= 16900)
+        {
+            // animation to alert user as to what's going on
+            tot_inf_loop_blocks++;
+            printf("\b\b\b\b%4d", tot_inf_loop_blocks);
+        }
+    } while (!reset_grid(grid));
+
+    // for part 1
     // find total number trodden tiles
-    uint16_t tot = find_tot_tiles_trodden(grid);
-    printf("TOTAL TILES TRODDEN: %d", tot);
+    // uint16_t tot = find_tot_tiles_trodden(grid);
+    // printf("TOTAL TILES TRODDEN: %d\n", tot);
 
     // free grid
     uint8_t row = 0;
--- day6/main.h
+++ day6/main.h
@@ -3,20 +3,36 @@
 
 #include <stdint.h>
 
+// used for denoting the direction of the guard as they move though the grid
 typedef enum DIRECTION {
     INVALID = 0, // value for if guard is not changing directions
     UP,
     RIGHT,
     DOWN,
     LEFT,
+    INF, // for candidacy checks
     EXIT, // value for if guard is at an exit
 } DIRECTION;
 
+// used for denoting spaced on the grid
 typedef enum TILE {
     EMPTY = '.',
     BLOCKED = '#',
     GUARD = '^',
-    TRODDEN = 'X',
+    TRED = 'X',
+    INF_BLOCK = 'O',
 } TILE;
+
+/**
+ * candidate check
+ */
+typedef struct candidate {
+    uint16_t steps_taken; // holds the steps a guard has taken to help check for inf loops
+    uint8_t col; // the current column the space we are checking is on
+    uint8_t row; // the current row the current space we are checking is on
+    uint8_t guard_init_row; // the initial position of the gurad
+    uint8_t guard_init_col; // the initial position of the gurad
+    DIRECTION direction; // the direction of the guard was IF they encountered the tile. INVALID otherwise. INF if the guard ended up back here
+} candidate;
 
 #endif
