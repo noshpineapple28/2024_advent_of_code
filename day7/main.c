a81c948ce53d23d71bf72acf098587c5df7849f5
noshpineapple28 <manoucherin@gmail.com>
2024-12-25 09:33:41 -0800

    initial commit

---
+++ day7/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+// global vars
+
+// helper methods
+
+// main methods
+
+/**
+ * runs advent of code day 7
+ */
+void main()
+{
+    // open file
+    FILE *fp;
+    fp = fopen("./input", "r");
+
+    // close file!
+    fclose(fp);
+}
